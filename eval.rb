a753e1b0ab692ebb27ae5ad85a0bf83167d2c5b0
Siwei Shen <sg552sg552@gmail.com>
2012-08-11 08:58:43 +0800

    added instance_eval example

--- eval.rb
+++ eval.rb
@@ -4,6 +4,11 @@ CODE
 eval(string)
 
 class Apple; end
+
+apple_string = "apple"
+apple_string.instance_eval %Q{ def say_hi; puts 'hi, from apple'; end }
+apple_string.say_hi
+
 Apple.class_eval(%Q{ def say_hi; puts 'hi'; end }, "dummy.rb", 123)
 Apple.class_eval(%Q{ def say_error; raise 'error'; end }, "dummy.rb", 123)
 Apple.new.say_hi
